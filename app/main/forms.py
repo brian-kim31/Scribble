b5d59409d407a9b9f5213b5c468539e00d87a82a
brayokenya <kiirubrian21@gmail.com>
2020-06-08 23:13:00 +0300

    ädded styling

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,7 @@ class PitchForm(FlaskForm):
 
     title = StringField('Title', validators=[Required()])
     post = TextAreaField('Pitch', validators=[Required()])
-    category = SelectField('Category', choices=[('product', 'product'), ('idea', 'idea'), ('business', 'business')],
+    category = SelectField('Category', choices=[('Product', 'Product'), ('Idea', 'Idea'), ('Business', 'Business')],
                            validators=[Required()])
     submit = SubmitField('Post')
 
